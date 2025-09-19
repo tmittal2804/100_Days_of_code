4f2e8151e901aa6b3b03cf508210b56bb044878b
tmittal2804 <tmittal6960@gmail.com>
2025-09-20 00:53:35 +0530

    code

---
+++ Day_05/Hour.c
@@ -0,0 +1,15 @@
+// Write a program to input time in seconds and convert it to hours:minutes:seconds format.
+
+#include <stdio.h>
+int main() {
+    int totalSeconds, hours, minutes, seconds;
+    scanf("%d", &totalSeconds);
+    // Conversion
+    hours = totalSeconds / 3600;           
+    minutes = (totalSeconds % 3600) / 60;  
+    seconds = totalSeconds % 60;            
+    printf("%02d:%02d:%02d\n", hours, minutes, seconds);
+    return 0;
+}
+
+
---
+++ Day_05/Interest.c
@@ -0,0 +1,19 @@
+// Write a program to calculate simple and compound interest for given principal, rate, and time.
+
+#include <stdio.h>
+#include <math.h>
+int main() {
+    double principal, rate, time;
+    double simpleInterest, compoundInterest;
+    printf ("Enter the principal amount:\n");
+    scanf ("%lf", &principal);
+    printf ("Enter Rate:\n");
+    scanf ("%lf", &rate);
+    printf ("Enter time:\n");
+    scanf ("%lf", &time);
+    simpleInterest = (principal * rate * time) / 100;
+    compoundInterest = principal * (pow((1 + rate / 100.0), time)) - principal;
+    printf("Simple Interest=%.2lf, Compound Interest=%.2lf\n", simpleInterest, compoundInterest);
+   return 0;
+}
+
