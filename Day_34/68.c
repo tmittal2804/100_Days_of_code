496427728068ea30b6ae4439bc131f8aa7a6f26b
tmittal2804 <tmittal6960@gmail.com>
2025-09-23 19:14:37 +0530

    code

---
+++ Day_34/68.c
@@ -0,0 +1,20 @@
+// Delete an element from an array.
+
+#include <stdio.h>
+int main() {
+    int n, arr[100], pos;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    scanf("%d", &pos);
+    for (int i = pos; i < n - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+    n--; // reduce size after deletion
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
+
