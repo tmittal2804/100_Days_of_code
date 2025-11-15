2e6513e763206151e611c515a8e01edf4e0b87af
tmittal2804 <tmittal6960@gmail.com>
2025-11-15 23:26:23 +0530

    code

---
+++ Day_41/82.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin); 
+
+    printf("Characters in the string:\n");
+    for (int i = 0; str[i] != '\0' && str[i] != '\n'; i++) {
+        printf("%c\n", str[i]);
+    }
+
+    return 0;
+}
